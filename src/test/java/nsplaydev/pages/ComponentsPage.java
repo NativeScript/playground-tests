bf3f46618737db58691926382f149807f89b5630
vhristov5555 <vasil.hristov@progress.com>
2017-09-25 14:50:50 +0300

    Add NSPlayDevTests.

---
+++ src/test/java/nsplaydev/pages/ComponentsPage.java
@@ -0,0 +1,63 @@
+package nsplaydev.pages;
+
+import functional.tests.core.mobile.basepage.BasePage;
+import functional.tests.core.mobile.element.UIElement;
+import io.appium.java_client.SwipeElementDirection;
+import org.testng.Assert;
+
+public class ComponentsPage extends BasePage {
+
+    public ComponentsPage() throws InterruptedException {
+        super();
+        UIElement browse = this.wait.waitForVisible(this.locators.findByTextLocator("Components", true));
+        if(browse==null)
+        {
+            this.navigateBack();
+            this.waitForElement(500);
+            browse = browse = this.find.byText("Components");
+        }
+        if(browse==null)
+        {
+            this.navigateBack();
+            this.waitForElement(500);
+            browse = this.find.byText("Components");
+        }
+        Assert.assertNotNull(browse, "Components page not loaded!");
+        this.log.info("Components page loaded.");
+    }
+
+    /**
+     * Verify home page loaded.
+     */
+    public void navigate(String button) {
+        this.find.byText(button).click();
+        this.log.info("Navigate to " + button);
+    }
+
+    public boolean checkIfElementisShown(String elementText) {
+        boolean isElementFound = false;
+        UIElement element = this.find.byText(elementText);
+        if(element != null)
+        {
+            isElementFound = true;
+            this.log.info("Item " + elementText + " found!");
+        }
+        else
+        {
+            this.log.info("Item " + elementText + " not found!");
+        }
+        return isElementFound;
+    }
+
+    public void waitForElement(int time) throws InterruptedException {
+        synchronized(this.wait) {
+            this.wait.wait(time);
+        }
+    }
+
+    public void scrollDown() {
+        UIElement scroll = this.wait.waitForVisible(this.locators.findByTextLocator("DatePicker", true));
+        scroll.scrollInElement(SwipeElementDirection.DOWN, 1);
+        this.log.info("Scroll Down");
+    }
+}
---
+++ src/test/java/nsplaydev/pages/ComponentsVisualizationPage.java
@@ -0,0 +1,55 @@
+package nsplaydev.pages;
+
+import functional.tests.core.mobile.basepage.BasePage;
+import functional.tests.core.mobile.element.UIElement;
+import io.appium.java_client.SwipeElementDirection;
+import org.testng.Assert;
+
+public class ComponentsVisualizationPage extends BasePage {
+
+    public ComponentsVisualizationPage(String component) {
+        super();
+        UIElement browse = this.wait.waitForVisible(this.locators.findByTextLocator(component, true));
+        if(browse== null) {
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            this.scrollDown();
+            browse = this.wait.waitForVisible(this.locators.findByTextLocator(component, true));
+        }
+        Assert.assertNotNull(browse, "Components Visualization page not loaded!");
+        this.log.info("Components Visualization page loaded.");
+    }
+
+    /**
+     * Verify home page loaded.
+     */
+    public void navigate(String button) {
+        this.find.byText(button).click();
+        this.log.info("Navigate to " + button);
+    }
+
+    public void navigateBackPage() {
+        this.navigateBack();
+    }
+
+    public void scrollDown() {
+        UIElement scroll = this.wait.waitForVisible(this.locators.findByTextLocator("DatePicker", true));
+        scroll.scrollInElement(SwipeElementDirection.DOWN, 1);
+        this.log.info("Scroll Down");
+    }
+}
