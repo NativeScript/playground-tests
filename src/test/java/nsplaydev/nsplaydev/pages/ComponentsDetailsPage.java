11978e2f0d0df8edfc0ca7a6e70d15df7419e5d8
vhristov5555 <vasil.hristov@progress.com>
2017-09-28 15:12:06 +0300

    Add Ipad images.

---
+++ src/test/java/nsplaydev/nsplaydev/pages/ComponentsDetailsPage.java
@@ -0,0 +1,27 @@
+package nsplaydev.nsplaydev.pages;
+
+import functional.tests.core.mobile.basepage.BasePage;
+import functional.tests.core.mobile.element.UIElement;
+import org.testng.Assert;
+
+public class ComponentsDetailsPage extends BasePage {
+
+    public ComponentsDetailsPage() {
+        super();
+        UIElement browse = this.wait.waitForVisible(this.locators.findByTextLocator("Details", true));
+        Assert.assertNotNull(browse, "Details page not loaded!");
+        this.log.info("Details page loaded.");
+    }
+
+    /**
+     * Verify home page loaded.
+     */
+    public void navigate(String button) {
+        this.find.byText(button).click();
+        this.log.info("Navigate to " + button);
+    }
+
+    public void navigateBackPage() {
+        this.navigateBack();
+    }
+}
