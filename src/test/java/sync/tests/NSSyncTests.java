42f47cbd9f60a98c6442fac71ae5e98e67fbe7d6
vhristov5555 <vasil.hristov@progress.com>
2018-04-12 11:23:48 +0300

    Fix dependencies.

--- src/test/java/sync/tests/NSSyncTests.java
+++ src/test/java/sync/tests/NSSyncTests.java
@@ -128,25 +128,25 @@ public class  NSSyncTests extends MobileTest {
                 "  \"@angular/platform-browser\": \"5.2.0\",\n" +
                 "  \"@angular/platform-browser-dynamic\": \"5.2.0\",\n" +
                 "  \"@angular/router\": \"5.2.0\",\n" +
-                "  \"tns-core-modules\": \"3.4.1\",\n" +
+                "  \"tns-core-modules\": \"4.0.0\",\n" +
                 "  \"nativescript-angular\": \"5.2.0\",\n" +
                 "  \"nativescript-ui-chart\": \"3.5.0\",\n" +
-                "  \"nativescript-ui-listview\": \"3.5.1\",\n" +
+                "  \"nativescript-ui-listview\": \"3.5.4\",\n" +
                 "  \"nativescript-ui-sidedrawer\": \"3.5.1\",\n" +
-                "  \"nativescript-ui-calendar\": \"3.5.0\",\n" +
+                "  \"nativescript-ui-calendar\": \"3.5.2\",\n" +
                 "  \"nativescript-ui-autocomplete\": \"3.6.0\",\n" +
                 "  \"nativescript-ui-dataform\": \"3.5.0\",\n" +
                 "  \"nativescript-ui-gauge\": \"3.5.0\",\n" +
-                "  \"kinvey-nativescript-sdk\": \"3.10.0\",\n" +
+                "  \"kinvey-nativescript-sdk\": \"3.10.2\",\n" +
                 "  \"nativescript-accelerometer\": \"2.0.1\",\n" +
                 "  \"nativescript-fresco\": \"3.0.6\",\n" +
                 "  \"nativescript-geolocation\": \"4.2.3\",\n" +
-                "  \"nativescript-iqkeyboardmanager\": \"1.2.0\",\n" +
+                "  \"nativescript-iqkeyboardmanager\": \"1.3.0\",\n" +
                 "  \"nativescript-intl\": \"3.0.0\",\n" +
-                "  \"nativescript-camera\": \"4.0.0\",\n" +
+                "  \"nativescript-camera\": \"4.0.2\",\n" +
                 "  \"nativescript-social-share\": \"1.5.0\",\n" +
                 "  \"nativescript-theme-core\": \"1.0.4\",\n" +
-                "  \"nativescript-vue\": \"1.3.0\",\n" +
+                "  \"nativescript-vue\": \"1.3.1\",\n" +
                 "  \"reflect-metadata\": \"0.1.12\",\n" +
                 "  \"rxjs\": \"5.5.6\",\n" +
                 "  \"zone.js\": \"0.8.20\"\n" +
