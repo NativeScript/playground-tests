ce901b10c2e6e89506f119b361e437cdec8adea8
vhristov5555 <vasil.hristov@progress.com>
2018-08-08 17:28:28 +0300

    Fix new versions of packges.

--- src/test/java/sync/tests/NSSyncTests.java
+++ src/test/java/sync/tests/NSSyncTests.java
@@ -119,38 +119,38 @@ public class  NSSyncTests extends MobileTest {
         }
 
         String componentVersionsExpected = "{\n" +
-                "  \"@angular/animations\": \"5.2.0\",\n" +
-                "  \"@angular/common\": \"5.2.0\",\n" +
-                "  \"@angular/compiler\": \"5.2.0\",\n" +
-                "  \"@angular/core\": \"5.2.0\",\n" +
-                "  \"@angular/forms\": \"5.2.0\",\n" +
-                "  \"@angular/http\": \"5.2.0\",\n" +
-                "  \"@angular/platform-browser\": \"5.2.0\",\n" +
-                "  \"@angular/platform-browser-dynamic\": \"5.2.0\",\n" +
-                "  \"@angular/router\": \"5.2.0\",\n" +
-                "  \"tns-core-modules\": \"4.0.0\",\n" +
-                "  \"nativescript-angular\": \"5.3.0\",\n" +
-                "  \"nativescript-ui-chart\": \"3.6.0\",\n" +
-                "  \"nativescript-ui-listview\": \"3.5.4\",\n" +
-                "  \"nativescript-ui-sidedrawer\": \"4.0.0\",\n" +
-                "  \"nativescript-ui-calendar\": \"3.5.2\",\n" +
-                "  \"nativescript-ui-autocomplete\": \"3.6.0\",\n" +
-                "  \"nativescript-ui-dataform\": \"3.5.2\",\n" +
-                "  \"nativescript-ui-gauge\": \"3.5.0\",\n" +
-                "  \"kinvey-nativescript-sdk\": \"3.10.2\",\n" +
+                "  \"@angular/animations\": \"6.1.1\",\n" +
+                "  \"@angular/common\": \"6.1.1\",\n" +
+                "  \"@angular/compiler\": \"6.1.1\",\n" +
+                "  \"@angular/core\": \"6.1.1\",\n" +
+                "  \"@angular/forms\": \"6.1.1\",\n" +
+                "  \"@angular/http\": \"6.1.1\",\n" +
+                "  \"@angular/platform-browser\": \"6.1.1\",\n" +
+                "  \"@angular/platform-browser-dynamic\": \"6.1.1\",\n" +
+                "  \"@angular/router\": \"6.1.1\",\n" +
+                "  \"kinvey-nativescript-sdk\": \"3.11.6\",\n" +
                 "  \"nativescript-accelerometer\": \"2.0.1\",\n" +
-                "  \"nativescript-fresco\": \"3.0.6\",\n" +
-                "  \"nativescript-geolocation\": \"4.2.3\",\n" +
-                "  \"nativescript-iqkeyboardmanager\": \"1.3.0\",\n" +
-                "  \"nativescript-intl\": \"3.0.0\",\n" +
+                "  \"nativescript-angular\": \"6.1.0\",\n" +
+                "  \"nativescript-background-http\": \"3.3.0\",\n" +
                 "  \"nativescript-camera\": \"4.0.2\",\n" +
+                "  \"nativescript-fresco\": \"4.0.0\",\n" +
+                "  \"nativescript-geolocation\": \"4.3.0\",\n" +
+                "  \"nativescript-intl\": \"3.0.0\",\n" +
+                "  \"nativescript-iqkeyboardmanager\": \"1.3.0\",\n" +
                 "  \"nativescript-social-share\": \"1.5.0\",\n" +
-                "  \"nativescript-background-http\": \"3.2.2\",\n" +
                 "  \"nativescript-theme-core\": \"1.0.4\",\n" +
+                "  \"nativescript-ui-autocomplete\": \"3.8.0\",\n" +
+                "  \"nativescript-ui-calendar\": \"3.7.0\",\n" +
+                "  \"nativescript-ui-chart\": \"3.7.0\",\n" +
+                "  \"nativescript-ui-dataform\": \"3.6.2\",\n" +
+                "  \"nativescript-ui-gauge\": \"3.6.0\",\n" +
+                "  \"nativescript-ui-listview\": \"3.5.11\",\n" +
+                "  \"nativescript-ui-sidedrawer\": \"4.2.1\",\n" +
                 "  \"nativescript-vue\": \"1.3.1\",\n" +
                 "  \"reflect-metadata\": \"0.1.12\",\n" +
-                "  \"rxjs\": \"5.5.6\",\n" +
-                "  \"zone.js\": \"0.8.20\"\n" +
+                "  \"rxjs\": \"6.2.2\",\n" +
+                "  \"tns-core-modules\": \"4.2.0\",\n" +
+                "  \"zone.js\": \"0.8.26\"\n" +
                 "}";
         codeEditor.assertDeviceTab(this.deviceName, modelExpected, osVersionExpected, previewAppVersionExpected, runtimeVersionExpected, componentVersionsExpected);
     }
