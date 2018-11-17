dc0a882ccc7ce30f77e3a9771c0bb427779337ff
vhristov5555 <vasil.hristov@progress.com>
2018-11-17 12:54:12 +0200

    Test SIkuli click method.

--- src/test/java/sync/pages/CodeEditorClass.java
+++ src/test/java/sync/pages/CodeEditorClass.java
@@ -666,15 +666,11 @@ public class CodeEditorClass extends BasePage {
                 if(isValid) {
                     code = "<template>\n" +
                             "    <Page class=\"page\">\n" +
-                            "        <ActionBar title=\"Home\" class=\"action-bar\" />\n" +
+                            "        <ActionBar title=\"Test\" class=\"action-bar\" />\n" +
                             "        <ScrollView>\n" +
                             "            <StackLayout class=\"home-panel\">\n" +
                             "                <!--Add your page content here-->\n" +
-                            "                <Label textWrap=\"true\" text=\"Play with NativeScript!\" class=\"h2 description-label\" />\n" +
-                            "                <Label textWrap=\"true\" text=\"Write code in the editor or drag and drop components to build a NativeScript mobile application.\"\n" +
-                            "                    class=\"h2 description-label\" />\n" +
-                            "                <Label textWrap=\"true\" text=\"Scan the QR code with your mobile device and watch the changes sync live while you play with the code.\"\n" +
-                            "                    class=\"h2 description-label\" />\n" +
+                            "                <Label textWrap=\"true\" text=\"Testing Label!\" class=\"h2 description-label\" />\n" +
                             "            </StackLayout>\n" +
                             "        </ScrollView>\n" +
                             "    </Page>\n" +
@@ -706,15 +702,11 @@ public class CodeEditorClass extends BasePage {
                 else{
                     code = "<template>\n" +
                             "    <Page class=\"page\">\n" +
-                            "        <ActionBar title=\"Home\" class=\"action-bar\" />\n" +
+                            "        <ActionBar title=\"Test\" class=\"action-bar\" />\n" +
                             "        <ScrollView>\n" +
                             "            <StackLayout class=\"home-panel\">\n" +
                             "                <!--Add your page content here-->\n" +
-                            "                <Label textWrap=\"true\" text=\"Play with NativeScript!\" class=\"h2 description-label\" />\n" +
-                            "                <Label textWrap=\"true\" text=\"Write code in the editor or drag and drop components to build a NativeScript mobile application.\"\n" +
-                            "                    class=\"h2 description-label\" />\n" +
-                            "                <Label textWrap=\"true\" text=\"Scan the QR code with your mobile device and watch the changes sync live while you play with the code.\"\n" +
-                            "                    class=\"h2 description-label\" />\n" +
+                            "                <Label textWrap=\"true\" text=\"Testing Label!\" class=\"h2 description-label\" />\n" +
                             "            </StackLayout>\n" +
                             "        </ScrollView>\n" +
                             "    </Page>\n" +
@@ -910,15 +902,11 @@ public class CodeEditorClass extends BasePage {
         {
             String code = "<template>\n" +
                     "    <Page class=\"page\">\n" +
-                    "        <ActionBar title=\"Home\" class=\"action-bar\" />\n" +
+                    "        <ActionBar title=\"Test\" class=\"action-bar\" />\n" +
                     "        <ScrollView>\n" +
                     "            <StackLayout class=\"home-panel\">\n" +
                     "                <!--Add your page content here-->\n" +
-                    "                <Label textWrap=\"true\" text=\"Play with NativeScript!\" class=\"h2 description-label\" />\n" +
-                    "                <Label textWrap=\"true\" text=\"Write code in the editor or drag and drop components to build a NativeScript mobile application.\"\n" +
-                    "                    class=\"h2 description-label\" />\n" +
-                    "                <Label textWrap=\"true\" text=\"Scan the QR code with your mobile device and watch the changes sync live while you play with the code.\"\n" +
-                    "                    class=\"h2 description-label\" />\n" +
+                    "                <Label textWrap=\"true\" text=\"Testing Label!\" class=\"h2 description-label\" />\n" +
                     "            </StackLayout>\n" +
                     "        </ScrollView>\n" +
                     "    </Page>\n" +
@@ -1014,15 +1002,11 @@ public class CodeEditorClass extends BasePage {
         {
             String code = "<template>\n" +
                     "    <Page class=\"page\">\n" +
-                    "        <ActionBar title=\"Home\" class=\"action-bar\" />\n" +
+                    "        <ActionBar title=\"Test\" class=\"action-bar\" />\n" +
                     "        <ScrollView>\n" +
                     "            <StackLayout class=\"home-panel\">\n" +
                     "                <!--Add your page content here-->\n" +
-                    "                <Label textWrap=\"true\" text=\"Play with NativeScript!\" class=\"h2 description-label\" />\n" +
-                    "                <Label textWrap=\"true\" text=\"Write code in the editor or drag and drop components to build a NativeScript mobile application.\"\n" +
-                    "                    class=\"h2 description-label\" />\n" +
-                    "                <Label textWrap=\"true\" text=\"Scan the QR code with your mobile device and watch the changes sync live while you play with the code.\"\n" +
-                    "                    class=\"h2 description-label\" />\n" +
+                    "                <Label textWrap=\"true\" text=\"Testing Label!\" class=\"h2 description-label\" />\n" +
                     "            </StackLayout>\n" +
                     "        </ScrollView>\n" +
                     "    </Page>\n" +
@@ -1120,15 +1104,11 @@ public class CodeEditorClass extends BasePage {
         {
             String code = "<template>\n" +
                     "    <Page class=\"page\">\n" +
-                    "        <ActionBar title=\"Home\" class=\"action-bar\" />\n" +
+                    "        <ActionBar title=\"Test\" class=\"action-bar\" />\n" +
                     "        <ScrollView>\n" +
                     "            <StackLayout class=\"home-panel\">\n" +
                     "                <!--Add your page content here-->\n" +
-                    "                <Label textWrap=\"true\" text=\"Play with NativeScript!\" class=\"h2 description-label\" />\n" +
-                    "                <Label textWrap=\"true\" text=\"Write code in the editor or drag and drop components to build a NativeScript mobile application.\"\n" +
-                    "                    class=\"h2 description-label\" />\n" +
-                    "                <Label textWrap=\"true\" text=\"Scan the QR code with your mobile device and watch the changes sync live while you play with the code.\"\n" +
-                    "                    class=\"h2 description-label\" />\n" +
+                    "                <Label textWrap=\"true\" text=\"Testing Label!\" class=\"h2 description-label\" />\n" +
                     "            </StackLayout>\n" +
                     "        </ScrollView>\n" +
                     "    </Page>\n" +
