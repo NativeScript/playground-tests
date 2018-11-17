4a57120e115ac06f9beacb662a6c3baae805562d
vhristov5555 <vasil.hristov@progress.com>
2018-11-17 11:26:48 +0200

    Test SIkuli click method.

--- src/test/java/sync/pages/CodeEditorClass.java
+++ src/test/java/sync/pages/CodeEditorClass.java
@@ -251,6 +251,7 @@ public class CodeEditorClass extends BasePage {
                 this.typeCode("return {");
                 this.typeCode(Key.ENTER);
                 this.typeCode(Key.DOWN);
+                this.typeCode(Key.LEFT);
                 this.typeCode(";");
                 this.typeCode(Key.DOWN);
                 this.typeCode(",");
