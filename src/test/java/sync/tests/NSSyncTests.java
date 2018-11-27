22b7508910dc4c4d9fb778be66065331741311b4
vhristov5555 <vasil.hristov@progress.com>
2018-11-27 16:04:31 +0200

    Test SIkuli click method.

--- src/test/java/sync/tests/NSSyncTests.java
+++ src/test/java/sync/tests/NSSyncTests.java
@@ -24,7 +24,7 @@ public class  NSSyncTests extends MobileTest {
     @BeforeClass
     public void beforeClass() throws IOException, InterruptedException, FindFailed, UnsupportedFlavorException {
         this.setupClass = new SetupClass(this.client,this.settings, this.device);
-        String projectURL = "https://play.nativescript.be/?template=play-"+ setupClass.typeOfProject+"&debug=true&enableHMR=false";
+        String projectURL = "https://play.nativescript.org/?template=play-"+ setupClass.typeOfProject+"&debug=true&enableHMR=false";
         this.setupClass.getScreenShot("BeforeStartOfTests_BeforeNavigateToProject");
         this.setupClass.giveFocus();
         this.setupClass.NavigateToPage(projectURL);
